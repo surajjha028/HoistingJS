4ab6fe33474a707f7c04ae30d8a25fca62139845
Suraj Jha <121048122+surajjha028@users.noreply.github.com>
2023-06-16 09:27:43 +0530

    Let and Const Example file
    It files the contain the example of let and const for understanding purpose .

---
+++ hoisting_example3.js
@@ -0,0 +1,11 @@
+// Let Example 
+
+// console.log(a); It will give Reference Error
+
+// let a = 10;
+// var b = 10;
+
+// Const Example 
+
+// const a = 10; 
+// a = 2;         It will give TypeError
